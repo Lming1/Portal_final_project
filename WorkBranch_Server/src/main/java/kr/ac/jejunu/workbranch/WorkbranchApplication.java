dcc560b0c8726fd5d3c5e7cdad64ca471c0596eb
Lming1 <spear131121@gmail.com>
2018-06-01 20:20:47 +0900

    project repo setting

---
+++ WorkBranch_Server/src/main/java/kr/ac/jejunu/workbranch/WorkbranchApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.jejunu.workbranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbranchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkbranchApplication.class, args);
+    }
+}
