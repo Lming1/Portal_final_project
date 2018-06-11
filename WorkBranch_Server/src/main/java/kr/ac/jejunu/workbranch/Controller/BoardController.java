e5628cff5dd689466e37acda5062e4b60269477f
Lming1 <spear131121@gmail.com>
2018-06-11 20:10:48 +0900

    board update, delete, list

--- WorkBranch_Server/src/main/java/kr/ac/jejunu/workbranch/Controller/BoardController.java
+++ WorkBranch_Server/src/main/java/kr/ac/jejunu/workbranch/Controller/BoardController.java
@@ -6,6 +6,8 @@ import kr.ac.jejunu.workbranch.Repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/board")
 @RequiredArgsConstructor
@@ -21,6 +23,22 @@ public class BoardController {
     public Board create(@RequestBody Board board) {
         return boardRepository.save(board);
     }
+
+    @GetMapping("/list")
+    public List<Board> list() {
+        return boardRepository.findAll();
+    }
+
+    @PutMapping
+    public void modify(@RequestBody Board board) {
+        boardRepository.save(board);
+    }
+
+    @DeleteMapping("/{bid}")
+    public void delete(@PathVariable Integer bid) {
+        boardRepository.delete(boardRepository.findById(bid).get());
+    }
+
 
 
 
--- WorkBranch_Server/src/test/java/kr/ac/jejunu/workbranch/BoardTest.java
+++ WorkBranch_Server/src/test/java/kr/ac/jejunu/workbranch/BoardTest.java
@@ -3,6 +3,7 @@ package kr.ac.jejunu.workbranch;
 
 import kr.ac.jejunu.workbranch.Model.Board;
 import kr.ac.jejunu.workbranch.Model.Project;
+import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -73,5 +77,42 @@ public class BoardTest {
         board.setProject_id(project_id);
         board.setBDate(date);
         return restTemplate.postForObject(PATH, board, Board.class);
+    }
+
+    @Test
+    public void list() {
+        List<Board> boards = restTemplate.getForObject( PATH +"/list", List.class);
+        assertThat(boards, not(IsEmptyCollection.empty()));
+    }
+
+    @Test
+    public void modify() throws ParseException {
+        String title = "board_title";
+        String contents = "test_contents";
+        String email = "testuser@test.com";
+        Integer project_id = 1;
+        Board createdBoard = createBoard(title, contents, email, project_id);
+        createdBoard.setTitle("title1");
+        createdBoard.setContents("ggggggg");
+        restTemplate.put(PATH, createdBoard);
+        validate("title1", "ggggggg", email, createdBoard);
+    }
+
+    @Test
+    public void delete() throws ParseException {
+        String title = "board_title";
+        String contents = "test_contents";
+        String email = "testuser@test.com";
+        Integer project_id = 1;
+        Board createdBoard = createBoard(title, contents, email, project_id);
+        validate(title, contents, email, createdBoard);
+        restTemplate.delete(PATH + "/" + createdBoard.getBid());
+        Board board = restTemplate.getForObject(PATH + "/" + createdBoard.getBid(), Board.class);
+        assertThat(board.getBid(), is(nullValue()));
+        assertThat(board.getTitle(), is(nullValue()));
+        assertThat(board.getContents(), is(nullValue()));
+        assertThat(board.getEmail(), is(nullValue()));
+        assertThat(board.getProject_id(), is(nullValue()));
+        assertThat(board.getBDate(), is(nullValue()));
     }
 }
